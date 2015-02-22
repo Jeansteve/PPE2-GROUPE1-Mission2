e37e96a1fa9a883319a14dcf7d163a287462eae4
jeansteve <ekenjean@gmail.com>
2015-02-22 16:36:16 +0100

    Correction de la requete pour récupérer les détails des plateaux, ajout du block d'affichage des détails

--- index.php
+++ index.php
@@ -4,21 +4,24 @@ if(isset($_POST['num']))
     $num = $_POST['num'];
     $bdd = new PDO("mysql:host=localhost;dbname=BOCCO-GROUPE1","root","root");
     $req = $bdd->prepare("
-              SELECT PL.libelle_plateau, PL.prix_plateau, F.libellé_fromage, P.libellé_plat, E.libellé_entrée, D.Libelle_Dessert
-FROM PLATEAU PL, FROMAGE F, PLAT P, ENTREE E, DESSERT D
-WHERE PL.num_plateau = :num
-AND PL.code_fromage = F.Code_fromage
-AND PL.code_plat = P.Code_plat
-AND PL.code_entree = E.Code_entree
-AND PL.code_dessert = D.Code_Dessert") or die(print_r($bdd->errorInfo()));
+              SELECT PL.libelle_plateau, PL.prix_plateau, F.libelle_fromage, P.libelle_plat, E.libelle_entree, D.Libelle_Dessert
+                FROM PLATEAU PL, FROMAGE F, PLAT P, ENTREE E, DESSERT D
+                WHERE PL.num_plateau = :num
+                AND PL.code_fromage = F.Code_fromage
+                AND PL.code_plat = P.Code_plat
+                AND PL.code_entree = E.Code_entree
+                AND PL.code_dessert = D.Code_Dessert
+    ") or die(print_r($bdd->errorInfo()));
     $req->bindParam(":num", $num);
     $req->execute();
 
-    echo print_r($bdd->errorInfo());
-    $donnees = $req->fetchAll(PDO::FETCH_ASSOC);
+    //echo print_r($bdd->errorInfo());
+    $donnee = $req->fetch();
 
-    echo json_encode($donnees);
-    echo $num;
+   json_encode($donnee);
+
+    //echo $donnee['libelle_plateau'];
+
 }
 else
 {
@@ -41,19 +44,36 @@ else
         $req = $bdd->query("SELECT num_plateau, libelle_plateau, prix_plateau from PLATEAU");
         $donnees = $req->fetchAll();
 
-       json_encode($donnees);
+        json_encode($donnees);
 
         foreach($donnees as $plateau)
         {
             $num = $plateau['num_plateau'];
+            //Quand il clicque j'appelle la fonction pour obtenir les détails
             echo "<li onclick=\"getDetails(event, ".$num.")\" ><span class=\"titre\" >".$plateau['libelle_plateau']."</span>   <span class=\"prix\">".$plateau['prix_plateau']."€</span></li>";
         }
         ?>
     </ul>
+    <form method="post" name="formulaire">
+        <input type="text" name="num" value= "" hidden="hidden"/>
+    </form>
     <div id="details">
-        <form method="post" name="formulaire">
-            <input type="text" name="num" value= "" hidden="hidden"/>
-        </form>
+            <?php
+            //si il a sélectionné un plateau pour afficher les détails
+            if(isset($_POST['num']))
+            {
+                echo "<h2>Détails ".$donnee['libelle_plateau']."</h2>";
+                echo "<ul>";
+                foreach($donnee as $key => $elm)
+                {
+                    if(!is_integer($key)) // je vérifie si l'index n'est pas un entier car le résultat Json me retourne un index entier et une chaine
+                    {
+                        echo "<li>".utf8_encode($key)." = ".utf8_encode($elm)."</li>";
+                    }
+                }
+                echo "</ul>";
+            }
+            ?>
 
     </div>
 </div>
@@ -66,8 +86,6 @@ else
         document.formulaire.submit();
         //alert (event.target.parentNode.getAttribute("value"));
     }
-
-
 </script>
 </body>
 </html>
